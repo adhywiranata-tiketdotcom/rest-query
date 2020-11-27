a5dca17e76d064068315f8e57de00b6ee9b548cc
Adhy Wiranata Tiket <adhy.prasetyo@tiket.com>
2020-11-27 08:40:38 +0700

    Add react hook lint rules

--- src/utils/useEffectOnce.ts
+++ src/utils/useEffectOnce.ts
@@ -3,6 +3,7 @@ import * as React from 'react';
 type UseEffectHandler = (...args: any[]) => void
 
 function useEffectOnce(fn: UseEffectHandler) {
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   React.useEffect(fn, []);
 }
 
