1a2aa1856d3ff21dde93740ab226e9130112655e
Adhy Wiranata Tiket <adhy.prasetyo@tiket.com>
2020-11-28 20:37:28 +0700

    Add useFetchable hook to index map

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { default as RestQueryProvider } from './core/Provider';
 export { default as useCacheable } from './core/useCacheable';
+export { default as useFetchable } from './core/useFetchable';
 export { default as useStreamable } from './core/useStreamable';
