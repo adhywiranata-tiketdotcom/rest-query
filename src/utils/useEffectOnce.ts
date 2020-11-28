cb5484d37da51b34dba430579db28dfb9a516e48
Adhy Wiranata Tiket <adhy.prasetyo@tiket.com>
2020-11-28 20:07:50 +0700

    improve useEffectOnce function description

--- src/utils/useEffectOnce.ts
+++ src/utils/useEffectOnce.ts
@@ -2,6 +2,10 @@ import * as React from 'react';
 
 type UseEffectHandler = (...args: any[]) => void
 
+/**
+ * React useEffect which runs only once per component lifecycle
+ * @param fn effect function to run once
+ */
 function useEffectOnce(fn: UseEffectHandler) {
   // eslint-disable-next-line react-hooks/exhaustive-deps
   React.useEffect(fn, []);
