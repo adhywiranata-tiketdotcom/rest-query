592a2d4c32f1a9fc31a0f56555e594e2467ea5fe
Adhy Wiranata Tiket <adhy.prasetyo@tiket.com>
2020-11-30 21:04:43 +0700

    Improve docs on CachePersistor utility

--- src/utils/cachePersistor.ts
+++ src/utils/cachePersistor.ts
@@ -1,25 +1,43 @@
 import { CachedData, StoreHashMap } from '../interfaces';
 
-const CACHE_PERSISTOR_KEY_PREFIX = 'tix_restQueryPersistedCache';
+const CACHE_PERSISTOR_KEY_PREFIX = 'tix.rest.query.cache';
 const INITIAL_CACHE_VAL = '{}';
+const storageEngine = window.localStorage;
 
+/**
+ * CachePersistor works as a single source of truth - engine on cache persistence
+ * This pattern avoids any direct access to any other means of browser storage,
+ * hence, close for modification outside this particular class.
+ */
 abstract class CachePersistor {
+  /**
+   * Mutate the store based on changes on a single key
+   */
   static save(cacheKey: string, serializedData: CachedData) {
     const store = CachePersistor.getStore();
     store[cacheKey] = serializedData;
     CachePersistor.setStore(store);
   }
 
+  /**
+   * Get the value of store from the storage engine
+   */
   static getStore(): StoreHashMap {
-    return JSON.parse(window.localStorage.getItem(CACHE_PERSISTOR_KEY_PREFIX) || INITIAL_CACHE_VAL);
+    return JSON.parse(storageEngine.getItem(CACHE_PERSISTOR_KEY_PREFIX) || INITIAL_CACHE_VAL);
   }
 
+  /**
+   * Overwrite the value of store inside the storage engine
+   */
   static setStore(persistedCacheStore: StoreHashMap) {
-    window.localStorage.setItem(CACHE_PERSISTOR_KEY_PREFIX, JSON.stringify(persistedCacheStore));
+    storageEngine.setItem(CACHE_PERSISTOR_KEY_PREFIX, JSON.stringify(persistedCacheStore));
   }
 
+  /**
+   * Purge the cached data from the storage engine. This won't directly affect the context's store.
+   */
   static removeStore() {
-    window.localStorage.removeItem(CACHE_PERSISTOR_KEY_PREFIX);
+    storageEngine.removeItem(CACHE_PERSISTOR_KEY_PREFIX);
   }
 }
 
